dbcb72aafc645549bfcbd2cef74f016730856530
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-11-22 17:11:00 +0100

    seperate redux files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import './styles/styles.scss';
+import { addExpense, editExpense, removeExpense } from './actions/expenses';
+import { setTextFilter, setStartDate, setEndDate } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({
+  description: 'Water bill',
+  amount: 12500,
+}));
+
+store.dispatch(addExpense({
+  description: 'Gas bill',
+  amount: 1250,
+}));
+
+console.log(store.getState());
+
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
