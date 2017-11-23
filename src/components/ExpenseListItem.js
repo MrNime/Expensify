88ee7bfca6c8cffebca155221d21501cbbc1bad8
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-11-23 18:35:33 +0100

    filter input and remove button

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({
+  dispatch, id, description, amount, createdAt,
+}) => (
   <div>
     <h4>{description}</h4>
     <p>€{amount}</p>
     <p>Created at: {createdAt}</p>
-    <br />
+    <p>{id}</p>
+    <button
+      onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
