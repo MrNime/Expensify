a797cb41c5149a675c82b4b221eb1e190df698d4
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-11-24 17:31:14 +0100

    add/edit expense form

--- src/app.js
+++ src/app.js
@@ -6,32 +6,31 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import './styles/styles.scss';
 import { addExpense, editExpense, removeExpense } from './actions/expenses';
-import { setTextFilter, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import { setTextFilter, setStartDate, setEndDate } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
 store.dispatch(addExpense({
   description: 'Water bill',
-  amount: 912500,
+  amount: 4500,
+  createdAt: 50,
 }));
 
 store.dispatch(addExpense({
   description: 'Gas bill',
-  amount: 1250,
+  createdAt: 1000,
 }));
 
-console.log(store.getState());
+store.dispatch(addExpense({
+  description: 'Rent',
+  amount: 109500,
+  createdAt: -444,
+}));
 
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('gas'));
-}, 3000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
