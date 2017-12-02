04ec5e19eb5bf3bff8f2d81823104c181fffb8ad
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-12-02 15:31:01 +0100

    component snapshot testing

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ExpenseList from './ExpenseList';
+import ConnectedExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
   <div>
     <ExpenseListFilters />
-    <ExpenseList />
+    <ConnectedExpenseList />
   </div>
 );
 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,12 +3,11 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = props => (
-  <div>
-    <h1>ExpenseList</h1>
-    {props.expenses.map(expense => <ExpenseListItem {...expense} key={expense.id} />)}
-  </div>
-);
+export const ExpenseList = props => (props.expenses.length === 0 ? (
+  <p>No Expenses</p>
+) : (
+  <div>{props.expenses.map(expense => <ExpenseListItem {...expense} key={expense.id} />)}</div>
+));
 
 const mapStateToProps = state => ({
   expenses: selectExpenses(state.expenses, state.filters),
