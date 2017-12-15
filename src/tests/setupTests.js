76be3b5c5a67e715377f29cde08acf96dc9779be
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-12-15 16:24:43 +0100

    env variables

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -5,8 +5,11 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'moment/locale/nl-be';
 import numeral from 'numeral';
 import 'numeral/locales/nl-be';
+import DotEnv from 'dotenv';
 
 numeral.locale('nl-be');
+
+DotEnv.config({ path: '.env.test' });
 
 Enzyme.configure({
   adapter: new Adapter(),
