2f668b345a0bc80ecdc14060619b4e91ca7d70e9
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-01-01 16:34:52 +0100

    change sort-direction images

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -68,17 +68,17 @@ export class ExpenseListFilters extends Component {
             >
               {this.props.filters.sortDirection === 'descending' ? (
                 <svg
-                  className="DayPickerNavigation_svg__horizontal DayPickerNavigation_svg__horizontal_1"
-                  viewBox="0 0 1000 1000"
+                className="DayPickerNavigation_svg__horizontal DayPickerNavigation_svg__horizontal_1"
+                viewBox="0 0 1000 1000"
                 >
-                  <path d="M336.2 274.5l-210.1 210h805.4c13 0 23 10 23 23s-10 23-23 23H126.1l210.1 210.1c11 11 11 21 0 32-5 5-10 7-16 7s-11-2-16-7l-249.1-249c-11-11-11-21 0-32l249.1-249.1c21-21.1 53 10.9 32 32z" />
+                  <path d="M967.5 288.5L514.3 740.7c-11 11-21 11-32 0L29.1 288.5c-4-5-6-11-6-16 0-13 10-23 23-23 6 0 11 2 15 7l437.2 436.2 437.2-436.2c4-5 9-7 16-7 6 0 11 2 16 7 9 10.9 9 21 0 32z"></path>
                 </svg>
-              ) : (
+                ) : (
                 <svg
                   className="DayPickerNavigation_svg__horizontal DayPickerNavigation_svg__horizontal_1"
                   viewBox="0 0 1000 1000"
                 >
-                  <path d="M694.4 242.4l249.1 249.1c11 11 11 21 0 32L694.4 772.7c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210.1-210.1H67.1c-13 0-23-10-23-23s10-23 23-23h805.4L662.4 274.5c-21-21.1 11-53.1 32-32.1z" />
+                  <path d="M32.1 712.6l453.2-452.2c11-11 21-11 32 0l453.2 452.2c4 5 6 10 6 16 0 13-10 23-22 23-7 0-12-2-16-7L501.3 308.5 64.1 744.7c-4 5-9 7-15 7-7 0-12-2-17-7-9-11-9-21 0-32.1z" />
                 </svg>
               )}
             </button>
