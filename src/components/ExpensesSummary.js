3ead4c9cec4560e3c7c42e40302774c4561947be
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-01-12 22:35:55 +0100

    add savings to summary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,29 +3,34 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
-import selectExpensesTotal from '../selectors/expenses-total';
+import selectExpensesTotal, { savedTotal } from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({
   visibleExpenseCount,
   visibleExpenseTotal,
+  visibleExpenseSavings,
   invisibleExpenseCount,
   invisibleExpenseTotal,
   allExpenseCount,
   allExpenseTotal,
+  allExpenseSavings,
 }) => {
   const visibleExpenseWord = visibleExpenseCount === 1 ? 'expense' : 'expenses';
   const formattedVisibleExpenseTotal = numeral(visibleExpenseTotal / 100).format('$0,0.00');
+  const formattedVisibleExpenseSavings = numeral(visibleExpenseSavings / 100).format('$0,0.00');
   const invisibleExpenseWord = invisibleExpenseCount === 1 ? 'expense' : 'expenses';
   const formattedInvisibleExpenseTotal = numeral(invisibleExpenseTotal / 100).format('$0,0.00');
   const allExpenseWord = allExpenseCount === 1 ? 'expense' : 'expenses';
   const formattedAllExpenseTotal = numeral(allExpenseTotal / 100).format('$0,0.00');
+  const formattedAllExpenseSavings = numeral(allExpenseSavings / 100).format('$0,0.00');
   return (
     <div className="page-header">
       <div className="content-container">
         <div className="page-header__content">
           <h1 className="page-header__title">
             Viewing <span>{visibleExpenseCount}</span> {visibleExpenseWord} worth{' '}
-            <span>{formattedVisibleExpenseTotal}</span>
+            <span>{formattedVisibleExpenseTotal}</span>. Visible savings:{' '}
+            <span>{formattedVisibleExpenseSavings}</span>
           </h1>
           <div className="page-header__section">
             <Link className="button" to="/create">
@@ -38,7 +43,8 @@ export const ExpensesSummary = ({
               </div>
               <div className="page-header__total">
                 from <span>{allExpenseCount}</span> total {allExpenseWord} worth{' '}
-                <span>{formattedAllExpenseTotal}</span>
+                <span>{formattedAllExpenseTotal}</span>. Total saved:{' '}
+                <span>{formattedAllExpenseSavings}</span>
               </div>
             </h3>
           </div>
@@ -53,11 +59,13 @@ const mapStateToProps = (state) => {
   return {
     visibleExpenseCount: visibleExpenses.length,
     visibleExpenseTotal: selectExpensesTotal(visibleExpenses),
+    visibleExpenseSavings: savedTotal(visibleExpenses),
     invisibleExpenseCount: state.expenses.length - visibleExpenses.length,
     invisibleExpenseTotal:
       selectExpensesTotal(state.expenses) - selectExpensesTotal(visibleExpenses),
     allExpenseCount: state.expenses.length,
     allExpenseTotal: selectExpensesTotal(state.expenses),
+    allExpenseSavings: savedTotal(state.expenses),
   };
 };
 
--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,2 +1,7 @@
 export default expenses =>
   expenses.map(expense => expense.amount).reduce((acc, amount) => acc + amount, 0);
+
+export const savedTotal = expenses =>
+  expenses
+    .map(expense => expense.amount - (expense.paidAmount ? expense.paidAmount : 0))
+    .reduce((acc, savings) => acc + savings, 0);
